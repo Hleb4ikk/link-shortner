b3768c329a711c59a72700492976a021238867e6
Hleb4ikk <hlebor2006@mail.ru>
2025-09-19 09:00:42 +0300

    chore: table for links was created

---
+++ backend/src/database/schemas/linksTable.ts
@@ -0,0 +1,22 @@
+import * as p from 'drizzle-orm/pg-core';
+import { usersTable } from './usersTable';
+import { relations } from 'drizzle-orm';
+
+const linksTable = p.pgTable('links', {
+  id: p.uuid('id').primaryKey().defaultRandom(),
+  ownerId: p
+    .uuid('owner_id')
+    .notNull()
+    .references(() => usersTable.id, { onDelete: 'cascade' }),
+  shortLinkId: p.varchar('short_link_id', { length: 10 }).unique().notNull(),
+  url: p.varchar('url', { length: 255 }).notNull(),
+  createdAt: p.timestamp('created_at').defaultNow().notNull(),
+});
+
+const linksRelations = relations(linksTable, ({ one }) => ({
+  owner: one(usersTable, {
+    fields: [linksTable.ownerId],
+    references: [usersTable.id],
+  }),
+}));
+export { linksTable, linksRelations };
--- backend/src/database/schemas/usersTable.ts
+++ backend/src/database/schemas/usersTable.ts
@@ -1,9 +1,17 @@
+import { relations } from 'drizzle-orm';
 import * as p from 'drizzle-orm/pg-core';
+import { linksTable } from './linksTable';
 
-export const usersTable = p.pgTable('users', {
+const usersTable = p.pgTable('users', {
   id: p.uuid('id').primaryKey().defaultRandom(),
   email: p.varchar('email', { length: 255 }).notNull().unique(),
   hashedPassword: p.varchar('hashed_password', { length: 255 }).notNull(),
   createdAt: p.timestamp('created_at').defaultNow().notNull(),
   updatedAt: p.timestamp('updated_at').defaultNow().notNull(),
 });
+
+const usersRelations = relations(usersTable, ({ many }) => ({
+  links: many(linksTable),
+}));
+
+export { usersTable, usersRelations };
