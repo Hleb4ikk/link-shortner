a9ee531167b731d67b1f6fc603f13e9e16682d42
Hleb <hlebor2006@mail.ru>
2025-10-20 12:53:36 +0300

    not found page is ready

--- frontend/src/app/App.tsx
+++ frontend/src/app/App.tsx
@@ -5,10 +5,14 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import AppLayout from '../components/layout/AppLayout';
 import HomePage from '../components/pages/HomePage/HomePage';
 import LinksPage from '../components/pages/LinksPage/LinksPage';
+import AudiencePage from '../components/pages/AudiencePage/AudiencePage';
+import NotFoundPage from '../components/pages/NotFoundPage/NotFoundPage';
 
 const routes = [
   { path: '/', element: <HomePage /> },
   { path: '/links', element: <LinksPage /> },
+  { path: '/links/:id', element: <AudiencePage /> },
+  { path: '*', element: <NotFoundPage /> },
 ];
 
 function App() {
