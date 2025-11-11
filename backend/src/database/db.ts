667a9ef69be7577fcc53f631afe9771012bd4004
Глеб Нюнько <nunkogleb348@gmail.com>
2025-11-11 20:51:55 +0300

    db_termination error was fixed

--- backend/src/database/db.ts
+++ backend/src/database/db.ts
@@ -1,4 +1,17 @@
 import { appConfig } from 'configuration/appConfig';
 import { drizzle } from 'drizzle-orm/node-postgres';
+import { logError } from 'logger';
+import { Pool } from 'pg';
 
-export const db = drizzle(appConfig.dbUrl);
+const pool = new Pool({
+  connectionString: appConfig.dbUrl,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+});
+
+pool.on('error', (err) => {
+  logError('[Postgres Pool Error]', err.message);
+});
+
+export const db = drizzle(pool);
